17d1dd3ca57e021a0f052e243cb5cb8eb29a4a0e
cephalon-ro <rodri5202@hotmail.com>
2018-08-25 16:31:01 -0300

    added toggling script

---
+++ Assets/Creados/Scripts/TimeToggle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeToggle : MonoBehaviour {
+    public delegate void swapButton();
+    public static event swapButton onPress;
+	// Use this for initialization
+	void Start () {
+        onPress();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (onPress != null)
+                onPress();
+        }
+	}
+}
---
+++ Assets/Creados/Scripts/TimeToggleable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeToggleable : MonoBehaviour {
+    public bool status = true;
+    public GameObject presentToggle;
+    public GameObject pastToggle;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnEnable()
+    {
+        TimeToggle.onPress += Swap;
+    }
+
+    private void OnDisable()
+    {
+        TimeToggle.onPress -= Swap;
+    }
+
+    void Swap()
+    {
+        if (status)
+        {
+            status = false;
+            pastToggle.SetActive(true);
+            presentToggle.SetActive(false);
+        }
+        else
+        {
+            status = true;
+            pastToggle.SetActive(false);
+            presentToggle.SetActive(true);
+        }
+    }
+ }
