5c472a7e456c7bf046aeab7fc1e8a60cf4e3c1d1
Ismael Serrada <ismael.serrada@etermax.com.uy>
2018-08-25 17:20:49 -0300

    created game scene, added character sprites

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : PhysicsObject
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
 
-    private SpriteRenderer spriteRenderer;
+    public SpriteRenderer spriteRenderer;
     //private Animator animator;
 
     // Use this for initialization
