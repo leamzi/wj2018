32cf801d0ea1cbb43e8497442b4618227ad2fabe
Ismael Serrada <ismael.serrada@etermax.com.uy>
2018-08-25 23:19:47 -0300

    update scenes and stuff

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,59 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement : PhysicsObject
+namespace Assets.Scripts
 {
-    public float maxSpeed = 7;
-    public float jumpTakeOffSpeed = 7;
-    public int status;
-    public SpriteRenderer spriteRenderer;
-    //private Animator animator;
-
-    // Use this for initialization
-    void Awake()
+    public class PlayerMovement : PhysicsObject
     {
-        status = 0;
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+        public float maxSpeed = 7;
+        public float jumpTakeOffSpeed = 7;
+        public int status;
+        public SpriteRenderer spriteRenderer;
+        //private Animator animator;
 
-    protected override void ComputeVelocity()
-    {
-        Vector2 move = Vector2.zero;
-
-        move.x = Input.GetAxis("Horizontal");
-
-        if (grounded && move.x != 0)
-            status = 1;
-        else if (grounded && move.x == 0)
-            status = 0;
-
-        if (Input.GetButtonDown("Jump") && grounded)
+        // Use this for initialization
+        void Awake()
         {
-            velocity.y = jumpTakeOffSpeed;
-            status = 2;
-            Debug.Log("jumped");
+            status = 0;
+            spriteRenderer = GetComponent<SpriteRenderer>();
         }
-        //else if (Input.GetButtonUp("Jump"))
-        //{
-        //    if (velocity.y > 0)
-        //    {
-        //        velocity.y = velocity.y * 0.5f;
-        //        status = 2;
-        //        Debug.Log("Jumped");
-        //    }
-        //}
 
-        //bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
-        //if (flipSprite)
-        //{
-        //    spriteRenderer.flipX = !spriteRenderer.flipX;
-        //}
+        protected override void ComputeVelocity()
+        {
+            Vector2 move = Vector2.zero;
 
-        //animator.SetBool("grounded", grounded);
-        //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+            move.x = Input.GetAxis("Horizontal");
 
-        targetVelocity = move * maxSpeed;
+            if (grounded && move.x != 0)
+                status = 1;
+            else if (grounded && move.x == 0)
+                status = 0;
+
+            if (Input.GetButtonDown("Jump") && grounded)
+            {
+                velocity.y = jumpTakeOffSpeed;
+                status = 2;
+                Debug.Log("jumped");
+            }
+            //else if (Input.GetButtonUp("Jump"))
+            //{
+            //    if (velocity.y > 0)
+            //    {
+            //        velocity.y = velocity.y * 0.5f;
+            //        status = 2;
+            //        Debug.Log("Jumped");
+            //    }
+            //}
+
+            //bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+            //if (flipSprite)
+            //{
+            //    spriteRenderer.flipX = !spriteRenderer.flipX;
+            //}
+
+            //animator.SetBool("grounded", grounded);
+            //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+            targetVelocity = move * maxSpeed;
+        }
     }
 }
 
