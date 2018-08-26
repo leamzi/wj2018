8a984e6ef108a7cb221b6e3c07a5f1c3a0fb6403
cephalon-ro <rodri5202@hotmail.com>
2018-08-25 21:19:54 -0300

    added Animation controller.

---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour {
+    public PlayerMovement playerObj;
+    public Animator playerAnim;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (playerObj.status)
+        {
+            case 0:
+                playerAnim.Play("Idle");
+                Debug.Log("Playing Idle");
+                break;
+            case 1:
+                playerAnim.Play("Moving");
+                Debug.Log("Playing Moving");
+                break;
+            case 2:
+                playerAnim.Play("Jump");
+                Debug.Log("playing Jump");
+                break;
+        }
+
+    }
+ }
+        
+
