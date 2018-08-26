ffe7170a9e8afbe7e9e8837a3d1dbcfe4de9f7dc
German <german.cedres12@gmail.com>
2018-08-26 15:34:15 -0300

    sceneloader

--- Assets/Scripts/GameCore/Inventory.cs
+++ Assets/Scripts/GameCore/Inventory.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 
     public class Inventory : MonoBehaviour {
 
+        public int totalItems = 5;
         public List<Collectible> itemList = new List<Collectible>();
         public CanvasBar bars;
         public void UpdateAmount()
-        {
+         {
             bars.UpdateBars(itemList.Count);
             Debug.Log(itemList.Count);
             Debug.Log("asdasd");
         }
+
+        public bool CheckInvetoryFull()
+        {
+        Debug.Log(itemList.Count);
+            if (itemList.Count == totalItems)
+            {
+                return true;
+            }
+            else
+                return false;
+            }
     }
 
