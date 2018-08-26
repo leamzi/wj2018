4a7deeb5343623a7ac0deff4ba88f0ef8a979d4f
Ismael Serrada <ismael.serrada@etermax.com.uy>
2018-08-26 05:08:41 -0300

    Added main screen ui functionality

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class MainMenuUI : MonoBehaviour
+	{
+
+		public GameObject InstructionPanel;
+		public GameObject CreditsPanel;
+
+		public Button PlayBtn;
+		public Button InstructionsBtn;
+		public Button CreditsBtn;
+		public Button ExitBtn;
+
+		private const string GameplayScene = "Gameplay";
+
+
+		public void ClearAllPanels()
+		{
+			InstructionPanel.SetActive(false);
+			CreditsPanel.SetActive(false);
+		}
+
+		private void Start()
+		{
+			PlayBtn.onClick.AddListener(PlayGame);
+			InstructionsBtn.onClick.AddListener(ShowInstructions);
+			CreditsBtn.onClick.AddListener(ShowCredits);
+			ExitBtn.onClick.AddListener(ExitGame);
+		}
+
+		void ShowInstructions()
+		{
+			InstructionPanel.SetActive(true);
+		}
+
+		void ShowCredits()
+		{
+			CreditsPanel.SetActive(true);
+		}
+
+		void PlayGame()
+		{
+			SceneManager.LoadScene(GameplayScene);
+		}
+
+		void ExitGame()
+		{
+			Application.Quit();
+		}
+	}
+}
